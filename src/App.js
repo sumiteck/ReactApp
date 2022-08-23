aae5136bbeae1cce6d944c43ea778b33fc851207
sumitk68 <imsumitk1@gmail.com>
2022-08-23 16:45:26 -0400

    cart visible

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
+import { useState } from 'react';
+
 import Header from './components/Layout/Header';
 import Meals from './components/Meals/Meals';
 import CartProvider from './store/CartProvider';
 
 function App() {
+const [cartIsShown, setCartIsShown] = useState(false);
+
+function showCartHandler(){
+  setCartIsShown(true);
+
+}
+
+function hideCartHandler(){
+  setCartIsShown(false);
+
+}
+
   return (
     <CartProvider>
       <Header />
